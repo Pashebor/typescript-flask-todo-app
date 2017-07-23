ab741e6313303820f2d6e08f2635a6f0ebd8b87e
Pashebor <pashebor@gmail.com>
2017-07-23 21:44:54 +0400

    sdfdsgf

--- db_connect.py
+++ db_connect.py
@@ -2,7 +2,6 @@ import mysql.connector
 
 
 def connection():
-    conn = mysql.connector.connect(host='127.13.8.2:3306', user='adminehnDfyk', password='tX911lSkRCAU',
-                                   database='myflaskapp2')
-    c = conn.cursor()
-    return c, conn
+    conn = mysql.connector.connect(host='localhost', user='root', password='ltvmzyjd',
+                                   database='house_electric')
+    conn.close()
