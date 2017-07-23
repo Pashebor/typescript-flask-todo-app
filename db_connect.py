c3497d9e3734cf48740d95713eccd02324cd6bab
Pashebor <pashebor@gmail.com>
2017-07-23 21:48:51 +0400

    sdfdsgf

--- db_connect.py
+++ db_connect.py
@@ -1,7 +1,5 @@
-import mysql.connector
+
 
 
 def connection():
-    conn = mysql.connector.connect(host='localhost', user='root', password='ltvmzyjd',
-                                   database='house_electric')
-    conn.close()
+    return 0
