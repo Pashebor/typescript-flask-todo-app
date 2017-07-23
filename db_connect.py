7fff44b4a973bed88b661c5c8f9facd96478e62f
Pashebor <pashebor@gmail.com>
2017-07-23 20:58:10 +0400

    db connect added 3

--- db_connect.py
+++ db_connect.py
@@ -2,6 +2,6 @@ import MySQLdb
 
 
 def connection():
-    conn = MySQLdb.connect(host='localhost', user='adminehnDfyk', passwd='tX911lSkRCAU', db='myflaskapp2')
+    conn = MySQLdb.connect(host='127.13.8.2:3306', user='adminehnDfyk', passwd='tX911lSkRCAU', db='myflaskapp2')
     c = conn.cursor()
     return c, conn
