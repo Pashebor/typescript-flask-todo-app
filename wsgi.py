b1c3bf483dca8b73a723103e5258f90d1a723d36
Pashebor <pashebor@gmail.com>
2017-07-23 19:40:15 +0400

    testing routes

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,7 @@ except IOError:
 # line, it's possible required libraries won't be in your searchable path
 #
 
-from myFlaskApp import app as application
+from server import app as application
 
 #
 # Below for testing only
