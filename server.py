b1a7f94bf317afa400c25f123c6c2828a1c0cf99
pashebor <pashebor@gmail.com>
2017-07-25 00:18:46 +0300

    trying and trying

--- server.py
+++ server.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:ltvmzyjd@localhost/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://adminehnDfyk:tX911lSkRCAU@127.13.8.2:3306/myflaskapp2'
 db = SQLAlchemy(app)
 
 
+# 127.13.8.2:3306
+# log: adminehnDfyk
+# pass: tX911lSkRCAU
 class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column('id', db.Integer, primary_key=True)
@@ -27,6 +30,7 @@ def hi():
 @app.route('/test')
 def test():
     return render_template('test.html')
+
 
 if __name__ == '__main__':
     app.run()
