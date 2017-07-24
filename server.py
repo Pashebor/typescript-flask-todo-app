2edcacde119bca100726d9fae7ded54e3074dbc2
pashebor <pashebor@gmail.com>
2017-07-25 00:04:28 +0300

    trying and trying

--- server.py
+++ server.py
@@ -1,49 +1,32 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from json import j
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:ltvmzyjd@localhost/house_electric'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:ltvmzyjd@localhost/test'
 db = SQLAlchemy(app)
 
 
 class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column('id', db.Integer, primary_key=True)
-    first_name = db.Column('first_name', db.String)
-    last_name = db.Column('last_name', db.String)
-    date_created = db.Column('date_created', db.DateTime)
-    is_admin = db.Column('is_admin', db.Boolean)
-    num_points = db.Column('num_points', db.Integer)
-
-    def __init__(self, first_name, last_name, date_created, is_admin, num_points):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.date_created = date_created
-        self.is_admin = is_admin
-        self.num_points = num_points
+    first_name = db.Column('first_name', db.Unicode)
+    last_name = db.Column('last_name', db.Unicode)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def hi():
     users = Users.query.all()
-    rows = []
+    row = []
     for user in users:
-        rows.append(user)
+        row.append(user.first_name)
 
-        print(rows[0])
+    print (row[0])
     return render_template('hi.html')
 
 
 @app.route('/test')
 def test():
     return render_template('test.html')
-
-
-@app.route('/register')
-def register_page():
-    return 0
-
 
 if __name__ == '__main__':
     app.run()
